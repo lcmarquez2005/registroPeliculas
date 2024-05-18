38c4ed95895a8decf201d9c45ef16ddf3d8e4019
lcmarquez2005 <93497992+lcmarquez2005@users.noreply.github.com>
2024-05-17 22:42:20 -0600

    Creando la Columna de Director que faltaba

--- src/registropeliculas/Interfaz2.java
+++ src/registropeliculas/Interfaz2.java
@@ -150,21 +150,28 @@ public class Interfaz2 extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null}
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
             },
             new String [] {
-                "ID", "Name", "DateLaunch", "Category", "Duration"
+                "ID", "Name", "DateLaunch", "Director", "Category", "Duration"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class
+                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class
+            };
+            boolean[] canEdit = new boolean [] {
+                true, true, true, false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         jScrollPane1.setViewportView(jTable1);
